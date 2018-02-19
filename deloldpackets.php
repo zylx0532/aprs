8995c08a1e3b0fac03efd090022646224feb44e1
Zhang Huanjie <james@ustc.edu.cn>
2018-02-19 10:09:56 +0800

    deloldpackets/del old posaprspacket

--- deloldpackets.php
+++ deloldpackets.php
@@ -5,4 +5,7 @@ include "web/db.php";
 
 $q="delete from aprspacket where tm<=date_sub(now(), interval 2 day)";
 $result = $mysqli->query($q);
+
+$q="delete from posaprspacket where tm<=date_sub(now(), interval 2 day) limit 2000";
+$result = $mysqli->query($q);
 ?>

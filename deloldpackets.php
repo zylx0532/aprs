d89647cd1f38ef54e6804751d68d532857a642fd
Zhang Huanjie <james@ustc.edu.cn>
2018-02-19 10:12:48 +0800

    deloldpackets limit 2000

--- deloldpackets.php
+++ deloldpackets.php
@@ -3,7 +3,7 @@
 date_default_timezone_set("Asia/Shanghai");
 include "web/db.php";
 
-$q="delete from aprspacket where tm<=date_sub(now(), interval 2 day)";
+$q="delete from aprspacket where tm<=date_sub(now(), interval 2 day) limit 2000";
 $result = $mysqli->query($q);
 
 $q="delete from posaprspacket where tm<=date_sub(now(), interval 2 day) limit 2000";

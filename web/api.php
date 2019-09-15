e0cbd074b8664cb126bdfd8f68379ba522be5a1f
Zhang Huanjie <james@ustc.edu.cn>
2019-09-15 16:14:07 +0800

    api

--- web/api.php
+++ web/api.php
@@ -1,5 +1,9 @@
 <?php
 
+// /api/getlastcall/long/lat/delta 获取以lng/lat为中心点，delta周边的站点
+// /api/getlastpos/CALL 获取某站点最后位置
+// /api/gettrak/CALL 获取某站点轨迹
+
 include "db.php";
 
 function strtolat($glat) {
@@ -17,6 +21,11 @@ function strtolon($glon) {
 	return $lon;
 }
 
+date_default_timezone_set( 'Asia/Shanghai');
+$startdate=date_create();
+date_sub($startdate,date_interval_create_from_date_string("1 days"));
+$startdatestr=date_format($startdate,"Y-m-d 00:00:00");
+
 $url_array = explode('/', $_SERVER['REQUEST_URI']);
 array_shift($url_array);
 array_shift($url_array);
@@ -25,12 +34,6 @@ if ($url_array[0] == "getlastcall") {
 	$x = $url_array[1];
 	$y = $url_array[2];
 	$delta = $url_array[3];
-
-
-	date_default_timezone_set( 'Asia/Shanghai');
-	$startdate=date_create();
-	date_sub($startdate,date_interval_create_from_date_string("1 days"));
-	$startdatestr=date_format($startdate,"Y-m-d 00:00:00");
 
 	$q="select `call`,lat,lon,tm,concat(`table`,symbol),datatype from lastpacket where tm>? and lat<>'' and not lat like '0000.00%' order by `call`";
 	$stmt=$mysqli->prepare($q);
@@ -44,7 +47,7 @@ if ($url_array[0] == "getlastcall") {
 	while($stmt->fetch()) {
 		$lat = strtolat($glat);
         	$lon = strtolon($glon);
-		$icon = "img/".bin2hex($dts).".png";
+		$icon = "http://202.141.176.2/img/".bin2hex($dts).".png";
 		if( ($lon > $x - $delta) && ($lon < $x + $delta) && ($lat > $y -$delta) && ($lat < $y+$delta)) {
 			if($count!=0)
 				echo ",\n";
@@ -54,7 +57,8 @@ if ($url_array[0] == "getlastcall") {
         		echo "\"iconPath\": \"$icon\", ";
         		echo "\"callname\":\"$call\", ";
         		echo "\"latitude\": $lat, ";
-        		echo "\"longitude\": $lon }";
+        		echo "\"longitude\": $lon, ";
+			echo "\"timestamp\": $dtmstr }";
 			$count ++;
 		}
 	} 
@@ -63,31 +67,66 @@ if ($url_array[0] == "getlastcall") {
 	exit(0);
 }
 
-exit(0);
-$span=$_SESSION["span"];  	
-$q="select lat,lon,tm,concat(`table`,symbol),msg,datatype from posaprspacket where tm>? and `call`=? and lat<>'' and not lat like '0000.00%' order by tm limit 50000";
-$stmt=$mysqli->prepare($q);
-$stmt->bind_param("ss",$startdatestr,$call);
-$stmt->execute();
-$stmt->bind_result($glat,$glon,$dtmstr,$dts,$dmsg,$ddt);
-$stmt->store_result();
-while($stmt->fetch()) {
-	$lat = strtolat($glat);
-        $lon = strtolon($glon);
-	if($jiupian==1) {
-		$p=$mp->WGStoBaiDuPoint($lon,$lat);
-		$lon = $p->getX();
-		$lat = $p->getY();
-	} else if($jiupian==2) {
-		$p=$mp->ChinatoBaiDuPoint($lon,$lat);
-		$lon = $p->getX();
-		$lat = $p->getY();
-	} 
-	$icon = "img/".bin2hex($dts).".png";
-	$dmsg = urlmessage($call, $icon, $dtmstr, $dmsg, "", $ddt);
-	echo "addpathpoint(".$lon.",".$lat.",\"".$dmsg."\");\n";
-}
-	echo "setstation(\"".$call."\",".$lon.",".$lat.",\"".$icon."\",\n\"".$dmsg."\");\n";
-$stmt->close();
+if ($url_array[0] == "getlastpos") {
+	$call = $url_array[1];
 
+	$q="select lat,lon,tm,concat(`table`,symbol),datatype from lastpacket where tm>? and `call`=?";
+	$stmt=$mysqli->prepare($q);
+	$stmt->bind_param("ss",$startdatestr, $call);
+	$stmt->execute();
+	$stmt->bind_result($glat,$glon,$dtmstr,$dts,$ddt);
+	$stmt->store_result();
+	echo "{ ";
+	$count = 0;
+
+	while($stmt->fetch()) {
+		$lat = strtolat($glat);
+        	$lon = strtolon($glon);
+		$icon = "http://202.141.176.2/img/".bin2hex($dts).".png";
+		echo "\"id\": $count, "; 
+        	echo "\"iconPath\": \"$icon\", ";
+        	echo "\"callname\":\"$call\", ";
+        	echo "\"latitude\": $lat, ";
+        	echo "\"longitude\": $lon, ";
+		echo "\"timestamp\": $dtmstr }";
+		$count ++;
+		$stmt->close();
+		exit(0);
+	} 
+	echo "}";
+	exit(0);
+}
+
+if ($url_array[0] == "gettrack") {
+	$call = $url_array[1];
+
+	$q="select lat,lon,tm,concat(`table`,symbol),datatype from posaprspacket where tm>? and `call`=? and lat<>'' and not lat like '0000.00%' order by tm";
+	$stmt=$mysqli->prepare($q);
+	$stmt->bind_param("ss",$startdatestr,$call);
+	$stmt->execute();
+	$stmt->bind_result($glat,$glon,$dtmstr,$dts,$ddt);
+	$stmt->store_result();
+	echo "{ \"callname\": \"$call\", tracks: [";
+	$count = 0;
+
+	while($stmt->fetch()) {
+		$lat = strtolat($glat);
+        	$lon = strtolon($glon);
+		$icon = "http://202.141.176.2/img/".bin2hex($dts).".png";
+		if($count!=0)
+			echo ",\n";
+		else
+			echo "\n";
+		echo "{\"id\": $count, "; 
+       		echo "\"iconPath\": \"$icon\", ";
+       		echo "\"latitude\": $lat, ";
+       		echo "\"longitude\": $lon, ";
+		echo "\"timestamp\": $dtmstr }";
+		$count ++;
+	} 
+	$stmt->close();
+	echo "\n] }";
+	exit(0);
+}
+exit(0);
 ?>

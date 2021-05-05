cae6dab8110f632bfccc6724bf834ab9cda72833
Blood.Cold <webmaster@zhiqiang.name>
2021-05-05 19:15:43 +0800

    Update testutil.c

--- testutil.c
+++ testutil.c
@@ -22,7 +22,7 @@ testutil(char *str)
 }
 int main (int argc, char **argv) {
 	testutil("user BH1IZK-3 pass 23208 vers iGnss.cc A1 V1 SV20181228\r\nBH1IZK-3>APA1V1,WIDE1-1:!4004.55N/11617.86E&APRS 144.640MHz@1200bps\r\n");
-	testutil("BG6CQ-9>AP51WX:,3134.10123N/12020.10456E_000/003g006t064r000p000h71b10090,086,119,138,057,078,091,000051WS5 23.8V");
-	testutil("BG6CQ-9>APRS,TCPIP*:!/>Ez#l,q=> PulseModem A");
+	testutil("BI8CYW-6>AP51WX:,3134.10123N/12020.10456E_000/003g006t064r000p000h71b10090,086,119,138,057,078,091,000051WS5 23.8V");
+	testutil("BI8CYW-6>APRS,TCPIP*:!/>Ez#l,q=> PulseModem A");
 	exit(0);
 }
